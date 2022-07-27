411e0bdf31dd6843322fa66a2d318f3cbeeb288f
usogeneral <workjobstesis@gmail.com>
2022-07-27 12:32:43 -0500

    make it better

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const URL_SERVICIOS = 'https://jobstesis.herokuapp.com';
-//export const URL_SERVICIOS = 'https://backendtrabajos.herokuapp.com';
+//export const URL_SERVICIOS = 'https://jobstesis.herokuapp.com';
+export const URL_SERVICIOS = 'https://backendtrabajos.herokuapp.com';
